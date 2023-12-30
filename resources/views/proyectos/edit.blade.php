74993c4bd67fa14cfe85b038919feb6c74a0061b
cheverriaw21 <cheverriawg@gmail.com>
2023-12-29 23:43:03 -0600

    cambios en la vistas de las etiquetas

--- resources/views/proyectos/edit.blade.php
+++ resources/views/proyectos/edit.blade.php
@@ -14,7 +14,7 @@
                         @method('PUT') {{-- Utilizamos PUT para la actualización --}}
 
                         <div class="mb-4">
-                            <label for="NombreProyecto" class="block text-gray-700 text-sm font-bold mb-2">{{__('Nombre del Proyecto')}}</label>
+                            <label for="NombreProyecto" class="block text-gray-700 text-sm font-bold mb-2">{{__('Nombre del Proyecto:')}}</label>
                             <input type="text" name="NombreProyecto" id="NombreProyecto" value="{{ old('NombreProyecto', $proyecto->NombreProyecto) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" required>
                             @error('NombreProyecto')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
@@ -22,7 +22,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="fuenteFondos" class="block text-gray-700 text-sm font-bold mb-2">{{__('Fuente de Fondos')}}</label>
+                            <label for="fuenteFondos" class="block text-gray-700 text-sm font-bold mb-2">{{__('Fuente de Fondos:')}}</label>
                             <input type="text" name="fuenteFondos" id="fuenteFondos" value="{{ old('fuenteFondos', $proyecto->fuenteFondos) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700">
                             @error('fuenteFondos')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
@@ -30,7 +30,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="MontoPlanificado" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Planificado')}}</label>
+                            <label for="MontoPlanificado" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Planificado ($):')}}</label>
                             <input type="number" name="MontoPlanificado" id="MontoPlanificado" value="{{ old('MontoPlanificado', $proyecto->MontoPlanificado) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" min="0" required >
                             @error('MontoPlanificado')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
@@ -38,7 +38,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="MontoPatrocinado" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Patrocinado')}}</label>
+                            <label for="MontoPatrocinado" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Patrocinado ($):')}}</label>
                             <input type="number" name="MontoPatrocinado" id="MontoPatrocinado" value="{{ old('MontoPatrocinado', $proyecto->MontoPatrocinado) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" min="0" required>
                             @error('MontoPatrocinado')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
@@ -46,7 +46,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="MontoFondosPropios" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Fondos Propios')}}</label>
+                            <label for="MontoFondosPropios" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Fondos Propios ($):')}}</label>
                             <input type="number" name="MontoFondosPropios" id="MontoFondosPropios" value="{{ old('MontoFondosPropios', $proyecto->MontoFondosPropios) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" min="0" required>
                             @error('MontoFondosPropios')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
--- resources/views/proyectos/form.blade.php
+++ resources/views/proyectos/form.blade.php
@@ -12,7 +12,7 @@
                     <form action="{{ $action }}" method="POST">
                         @csrf
                         <div class="mb-4">
-                            <label for="NombreProyecto" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Nombre del Proyecto</label>
+                            <label for="NombreProyecto" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Nombre del Proyecto:</label>
                             <input type="text" name="NombreProyecto" id="NombreProyecto" class="w-full mt-1 p-2 border rounded-md" value="{{ old('NombreProyecto') }}" required autofocus />
                             @error('NombreProyecto')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -20,7 +20,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="fuenteFondos" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Fuente de Fondos</label>
+                            <label for="fuenteFondos" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Fuente de Fondos:</label>
                             <input type="text" name="fuenteFondos" id="fuenteFondos" class="w-full mt-1 p-2 border rounded-md" value="{{ old('fuenteFondos') }}" />
                             @error('fuenteFondos')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -28,7 +28,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="MontoPlanificado" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Monto Planificado</label>
+                            <label for="MontoPlanificado" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Monto Planificado ($):</label>
                             <input type="number" name="MontoPlanificado" id="MontoPlanificado" class="w-full mt-1 p-2 border rounded-md" value="{{ old('MontoPlanificado') }}" min="0" required />
                             @error('MontoPlanificado')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -36,7 +36,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="MontoPatrocinado" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Monto Patrocinado</label>
+                            <label for="MontoPatrocinado" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Monto Patrocinado ($):</label>
                             <input type="number" name="MontoPatrocinado" id="MontoPatrocinado" class="w-full mt-1 p-2 border rounded-md" value="{{ old('MontoPatrocinado') }}" min="0" required />
                             @error('MontoPatrocinado')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -44,7 +44,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="MontoFondosPropios" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Monto Fondos Propios</label>
+                            <label for="MontoFondosPropios" class="block text-sm font-semibold text-gray-800 dark:text-gray-800">Monto Fondos Propios ($):</label>
                             <input type="number" name="MontoFondosPropios" id="MontoFondosPropios" class="w-full mt-1 p-2 border rounded-md" value="{{ old('MontoFondosPropios') }}" min="0" required />
                             @error('MontoFondosPropios')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
