9d55cc611cad299ceb39fd59cf3242c7614d1714
cheverriaw21 <cheverriawg@gmail.com>
2023-12-29 22:37:49 -0600

    se creo eliminar y actualizar

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -48,6 +48,53 @@ class ProyectoController extends Controller
         return redirect()->route('proyectos.index');
     }
 
+    //mostrar
+    public function show(Proyecto $proyecto): Renderable
+    {
+        // Puedes cargar relaciones aquí si es necesario
+        return view('proyectos.show', compact('proyecto'));
+    }
+
+    //editar variables
+    public function edit(Proyecto $proyecto): Renderable
+    {
+        $title = __('Editar proyecto');
+        $action = route('proyectos.update', $proyecto);
+        $buttonText = __('Actualizar proyecto');
+
+        return view('proyectos.edit', compact('proyecto', 'title', 'action', 'buttonText'));
+    }
+
+    //actualizar
+    public function update(Request $request, Proyecto $proyecto): RedirectResponse
+    {
+        $request->validate([
+            'NombreProyecto' => 'required|string|max:100',
+            'fuenteFondos' => 'string|max:1000',
+            'MontoPlanificado' => 'required|numeric|min:0',
+            'MontoPatrocinado' => 'required|numeric|min:0',
+            'MontoFondosPropios' => 'required|numeric|min:0',
+        ]);
+
+        $proyecto->update([
+            'NombreProyecto' => $request->input('NombreProyecto'),
+            'fuenteFondos' => $request->input('fuenteFondos'),
+            'MontoPlanificado' => $request->input('MontoPlanificado'),
+            'MontoPatrocinado' => $request->input('MontoPatrocinado'),
+            'MontoFondosPropios' => $request->input('MontoFondosPropios'),
+        ]);
+
+        return redirect()->route('proyectos.index');
+    }
+
+    //eliminar
+    public function destroy(Proyecto $proyecto): RedirectResponse
+    {
+        $proyecto->delete();
+        return redirect()->route('proyectos.index');
+    }
+
+
 
 
 }
---
+++ resources/views/proyectos/edit.blade.php
@@ -0,0 +1,66 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $title }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ $action }}" method="POST">
+                        @csrf
+                        @method('PUT') {{-- Utilizamos PUT para la actualización --}}
+
+                        <div class="mb-4">
+                            <label for="NombreProyecto" class="block text-gray-700 text-sm font-bold mb-2">{{__('Nombre del Proyecto')}}</label>
+                            <input type="text" name="NombreProyecto" id="NombreProyecto" value="{{ old('NombreProyecto', $proyecto->NombreProyecto) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700">
+                            @error('NombreProyecto')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="fuenteFondos" class="block text-gray-700 text-sm font-bold mb-2">{{__('Fuente de Fondos')}}</label>
+                            <input type="text" name="fuenteFondos" id="fuenteFondos" value="{{ old('fuenteFondos', $proyecto->fuenteFondos) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700">
+                            @error('fuenteFondos')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="MontoPlanificado" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Planificado')}}</label>
+                            <input type="number" name="MontoPlanificado" id="MontoPlanificado" value="{{ old('MontoPlanificado', $proyecto->MontoPlanificado) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700">
+                            @error('MontoPlanificado')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="MontoPatrocinado" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Patrocinado')}}</label>
+                            <input type="number" name="MontoPatrocinado" id="MontoPatrocinado" value="{{ old('MontoPatrocinado', $proyecto->MontoPatrocinado) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700">
+                            @error('MontoPatrocinado')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="MontoFondosPropios" class="block text-gray-700 text-sm font-bold mb-2">{{__('Monto Fondos Propios')}}</label>
+                            <input type="number" name="MontoFondosPropios" id="MontoFondosPropios" value="{{ old('MontoFondosPropios', $proyecto->MontoFondosPropios) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700">
+                            @error('MontoFondosPropios')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        {{-- Botones --}}
+                        <div class="flex justify-end">
+                            <button type="submit" class="bg-green-700 hover:bg-green-950 text-white font-bold py-2 px-4 rounded mr-2" >{{ $buttonText }}</button>
+                            <a href="{{ route('proyectos.index') }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Cancelar</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
